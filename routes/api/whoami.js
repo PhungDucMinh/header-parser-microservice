a8ac93397105b350b550035e52602b0b60281551
DucMinh <phungducminh95@gmail.com>
2017-01-09 19:31:58 +0700

    update whoami.js return object

--- routes/api/whoami.js
+++ routes/api/whoami.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    console.log("ip: " + req.ip);
+    var ip =  req.connection.remoteAddress || req.headers['x-forwarded-for'];
+    var language = req.headers['accept-language'].split(',')[0];
+    var regex = /\([\w\W]+?\)/g
+    var software = req.headers['user-agent'].match(regex)[0].replace(/[\(\)]/g, "");
+    console.log({
+        ipaddress: ip,
+        language: language,
+        software: software
+    });
     res.send(req.headers);
 });
 
