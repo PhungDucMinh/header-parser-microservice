49e07b3a5dff027ef280577a0a593bc993756144
DucMinh <phungducminh95@gmail.com>
2017-01-09 19:36:32 +0700

    update whoami return object ip

--- routes/api/whoami.js
+++ routes/api/whoami.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    var ip =  req.connection.remoteAddress || req.headers['x-forwarded-for'];
+    var ip =  req.headers['x-forwarded-for'];
     var language = req.headers['accept-language'].split(',')[0];
     var regex = /\([\w\W]+?\)/g
     var software = req.headers['user-agent'].match(regex)[0].replace(/[\(\)]/g, "");
